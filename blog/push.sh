0d14b2d7ce72f024a25d25a9c5b54eae8103dba0
cnlinjie <cnlinjie@foxmail.com>
2015-07-03 10:12:38 +0800

    测试自动提交

--- blog/push.sh
+++ blog/push.sh
@@ -5,8 +5,6 @@ read -p "是否提交源文件到github上(y/n) default(n):" if_commit
 if [ "$if_commit" = "" ]; then
 	if_commit="n"
 fi
-
-echo $if_commit
 if [ "$if_commit" = "y" ]; then
 	read -p "输入提交信息:" git_message
 	if [ "$git_message" = "" ]; then
--- blog/re.sh
+++ blog/re.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 if_commit=""
-read -p "是否提交源文件到github上(y/n) default(n)": if_commit
-if [ "if_commit" = "" ]; then
-   if_commit="n"
+git_message=""
+read -p "是否提交源文件到github上(y/n) default(n):" if_commit
+if [ "$if_commit" = "" ]; then
+	if_commit="n"
+fi
+if [ "$if_commit" = "y" ]; then
+	read -p "输入提交信息:" git_message
+	if [ "$git_message" = "" ]; then
+		git_message = "没有提交信息"
+	fi
 fi
 
+echo "-----------开始发布:----------"
 hexo g -d 
+
+if [ "$if_commit" = "y" ]; then
+	echo "---------------发布成功，开始提交源文件:---------------------"
+	git add .
+	git commit -m "$git_message"
+	git push
+fi
 
